285137834090f0a188e09ad0f8a09a355f01b97a
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-03 23:23:56 -0300

    add initial components and styles for menu-bar, hero-card, notice-card, and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { TitleComponent } from "./components/title/title.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
