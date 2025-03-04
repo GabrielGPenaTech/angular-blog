8eac75e31bc1112cd1e75c715ed835d0ea3a9e73
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-04 06:03:11 -0300

    add menu bar component and update home layout; refactor hero card and notice card for improved styling and responsiveness

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TitleComponent } from "../../components/title/title.component";
 import { HeroCardComponent } from "../../components/hero-card/hero-card.component";
 import { NoticeCardComponent } from "../../components/notice-card/notice-card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TitleComponent, HeroCardComponent, NoticeCardComponent],
+  imports: [TitleComponent, HeroCardComponent, NoticeCardComponent, MenuBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
