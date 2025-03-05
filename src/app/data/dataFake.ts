befd5240252bddb06077de2a4efd8ed7aac8e79c
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-04 23:38:21 -0300

    refactor fakeDB to include existing data and streamline data structure

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -25,27 +25,7 @@ export const data: CardProps[] = [
 ]
 
 export const fakeDB: CardProps[] = [
-  {
-    id: '1',
-    image: 'https://cpv.ifsp.edu.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png',
-    span: 'acho que é a data',
-    title: 'Pinguins engraçados no artico',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, quas, id, inventore aspernatur nam officiis reiciendis facere voluptates repudiandae ipsam hic rem. Asperiores aperiam eius earum, quaerat soluta nobis labore.'
-  },
-  {
-    id: '2',
-    image: 'https://s2-techtudo.glbimg.com/ZfasOR07ZFVKm1m4uSVcrY7eMmU=/0x0:1024x1024/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2024/f/f/GObKucQ4GJBz3ZJwaatQ/craiyon.png',
-    span: 'acho que é a data',
-    title: 'Raposa com olhar vibrante',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, quas, id, inventore aspernatur nam officiis reiciendis facere voluptates repudiandae ipsam hic rem. Asperiores aperiam eius earum, quaerat soluta nobis labore.'
-  },
-  {
-    id: '3',
-    image: 'https://st.depositphotos.com/2656399/3076/i/450/depositphotos_30765229-stock-photo-owl-watches-intently-illuminated-by.jpg',
-    span: 'acho que é a data',
-    title: 'Coruja sinistra olhando para você',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, quas, id, inventore aspernatur nam officiis reiciendis facere voluptates repudiandae ipsam hic rem. Asperiores aperiam eius earum, quaerat soluta nobis labore.'
-  },
+  ...data,
   {
     id: '4',
     image: 'https://t.ctcdn.com.br/7c-lFbP26Q_amYfHXgsUqW2vTLw=/640x360/smart/i325370.jpeg',
